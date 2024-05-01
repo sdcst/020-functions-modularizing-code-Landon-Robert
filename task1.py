260242abb1202f467dbb46b082c46e9e77b87cd3
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2024-05-01 13:34:17 -0700

    Finished

--- task1.py
+++ task1.py
@@ -1,6 +1,16 @@
 def A():
-
+    print("Hello")
 def B():
-
-
+    print("How are you")
 def C():
+    print("Hi")
+
+x = input("Pick a capital letter from A to C: ")
+if x == "A":
+    A()
+elif x == "B":
+    B()
+elif x == "C":
+    C()
+elif x != "A" and x != "B" and x != "C":
+    print("That is not a capital letter from A to C")
--- task2.py
+++ task2.py
@@ -8,3 +8,20 @@ displays instructions and how to play
 game()
 plays the games
 """
+
+def title():
+    print("Tutorial: use WASD to move, space to jump, left click to attack, right click to interact.")
+    print("Just kidding. Try to guess the number.")
+def game():
+    import random
+    x = random.randint(1,10)
+    attempts = 0
+    y = 0
+    while x != y:
+        y = int(input("\nPick a number from 1 to 10: "))
+        attempts = attempts + 1
+    else:
+        print(f"You guessed the number right, it took you {attempts} attempt(s)")
+
+title()
+game()
